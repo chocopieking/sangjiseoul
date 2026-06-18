554f1c5e0b7e692e6b8f3025c5838aba7dfa6e73
chocopieking <sogum25@gmail.com>
2026-06-18 21:39:22 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: { rollupOptions: { output: { manualChunks: { recharts:['recharts'], xlsx:['xlsx'] } } } }
+  build: { rollupOptions: { output: { manualChunks: { xlsx:['xlsx'] } } } }
 })
